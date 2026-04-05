d6136148ea0381be29220926249c162fbfa1cb78
sriayuwsb33-space <sriayuwsb33@gmail.com>
2026-04-05 12:58:23 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,12 @@ import { getFirestore, doc, setDoc, onSnapshot } from "firebase/firestore";
 import { getAuth, signInAnonymously, onAuthStateChanged } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCMHxO5hlxUBpZDuPa4PQkJ4EKIffzxqf8",
+  apiKey: "AIzaSyCMhxO5hlxUBpZDuPa4PQkJ4EkIFfzxqf8",
   authDomain: "toko-mas-sri-ayu.firebaseapp.com",
   projectId: "toko-mas-sri-ayu",
   storageBucket: "toko-mas-sri-ayu.firebasestorage.app",
   messagingSenderId: "195511507670",
-  appId: "1:195511507670:web:7c373958b65995d8f00777",
+  appId: "1:195511507670:web:7c373950b65995d8f00777",
   measurementId: "G-1BJ26ZSB0F"
 };
 
